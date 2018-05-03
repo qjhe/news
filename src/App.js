ecbd84fb526b0205a0618f6c62a595874a194823
qjhe <397459187@qq.com>
2018-05-04 07:45:45 +0800

    new  code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import {Router, Route} from 'react-router';
-import PCIndex from './components/pc_index';
-
-import createBrowserHistory from 'history/createBrowserHistory';
-const history = createBrowserHistory();
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Router history={history}>
-          <Route path="/" component={PCIndex}></Route>
-      </Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
